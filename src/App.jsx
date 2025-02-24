b6dc288b3fd8d22c8a5cc17324db59248d9ae5a0
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-24 12:44:38 +0100

    add: List comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // import { useState } from 'react'
 // import './App.css'
 
+import List from "./components/List";
+
 
 
 function App() {
@@ -9,7 +11,7 @@ function App() {
 
     <>
       <div className="min-h-screen w-full flex flex-col gap-4u justify-center items-center">
-        
+        <List />
       </div>
     </>
 
---
+++ src/components/List.jsx
@@ -0,0 +1,18 @@
+
+
+
+export default function List () {
+
+    return (
+
+        <>
+        <ul className="flex flex-col gap-3u">
+            <li>Ciao</li>
+            <li>Ciao</li>
+            <li>Ciao</li>
+        </ul>
+        </>
+
+    )
+
+}
