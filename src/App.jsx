c86f7bb051e3bf8c26553e7cd38ed100824ddfbd
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-24 14:40:07 +0100

    add: prevent default on form function

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import List from "./components/List";
 const articles = ["Titolo Art 1", "Titolo Art 2", "Titolo Art 3"];
 
 // function addNewArticle
-const addNewArticle = () => {
+const addNewArticle = (e) => {
+  e.preventDefault();
+
   return 1;
 }
 
@@ -28,13 +30,17 @@ function App() {
       <div className="min-h-screen w-full flex flex-col gap-4u justify-center items-center font-custom">
         <h1 className="font-h1">Lista di articoli</h1>
         {/* form */}
-        <form onSubmit={addNewArticle} className="w-[92%] max-w-[480px] flex flex-col justify-center items-center my-10u gap-2u">
+        <form onSubmit={ addNewArticle } className="w-[92%] max-w-[480px] flex flex-col justify-center items-center my-10u gap-2u">
+
           <label>Inserisci un nuovo articolo</label>
+
           <input className="border-2 border-smoke-200 rounded-full px-4u py-2u" type="text" placeholder="Tit nuovo articolo" onChange={ setArticleToValue } />
+
           <button type="submit">Inserisci</button>
+
         </form>
         {/* list */}
-        <List articles={articles} />
+        <List articles={articlesList} />
       </div>
     </>
 
