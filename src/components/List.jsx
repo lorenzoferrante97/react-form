53d0eb2157aa82c0c5757f46e37d361796fcd2b9
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-25 08:51:49 +0100

    bonus: test

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react"
 
 
 
 export default function List ({ articles }) {
+
+    // console.log("articles: ", articles)
+    
+    // const newArticles = articles;
+
+    // const [ updatedArticles, setUpdatedArticles ] = useState([]);
+
+    // console.log("updatedArticles: ", updatedArticles)
+
+    // const deleteFromList = (i) => {
+    //     setUpdatedArticles( articles.filter( (article, index) => index !== i ) );
+    // }
 
     return (
 
         <>
         <ul className="flex flex-col gap-3u w-full">
             {
+
                 articles.map( (article, index) => {
                     return (
-                        <li className="font-body-base-regular flex justify-between items-center" key={index}>
+                        <li className="font-body-base-regular flex justify-between items-center border-b-2 border-smoke-50 py-3u" key={index}>
                             <p>{article}</p>
-                            <button>X</button>
+                            {/* <button className="bg-red-400 text-white rounded-sm py-u px-2u hover:px-5u transition-all" onClick={ () => deleteFromList(index) }>X</button> */}
                         </li>
                     )
                 } )
